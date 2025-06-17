cce998fab3df021ba1eb9734b0db7ce38a1a0453
Pankaj Gaikwad <gaikwadpankaj62@gmail.com>
2025-06-17 18:03:35 +0530

    style: enhance Footer component layout and add customer service details

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,19 +2,46 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className='bg-[#383735] text-white px-16 py-16 grid grid-cols-4'>
-      <div className='flex-col space-y-4 text-lg'>
-        <div className='flex flex-col gap-2'>
-          <h4 className='text-lg font-bold uppercase'>Social Edge</h4>
+    <footer className='bg-[#383735] text-white '>
+      <div className='px-16 py-16 flex'>
+        {/*  */}
+        <div className='flex-col space-y-4 text-lg'>
+          <div className='flex flex-col gap-2'>
+            <h4 className='text-lg font-bold uppercase'>Social Edge</h4>
+            <p className='text-zinc-200'>
+              Social Edge is a boutique PR and Online Reputation Management
+              agency specializing in reputation repair and publicity enhancement
+              services
+            </p>
+          </div>
+          <p className='text-zinc-200'>All prices listed on site are in USD</p>
           <p className='text-zinc-200'>
-            Social Edge is a boutique PR and Online Reputation Management agency
-            specializing in reputation repair and publicity enhancement services
+            We are based at 8 The Green, Dover, DE 19901
           </p>
         </div>
-        <p className='text-zinc-200'>All prices listed on site are in USD</p>
-        <p className='text-zinc-200'>
-          We are based at 8 The Green, Dover, DE 19901
-        </p>
+        {/*  */}
+        <div className='flex gap'>
+          <div className='flex flex-col space-y-0.5'>
+            <h5 className='uppercase font-bold tracking-tight text-lg'>
+              customer service
+            </h5>
+            <p>Email Support</p>
+            <a href='#' className='text-zinc-200'>
+              support@maximaticmedia.com
+            </a>
+            <a href='#' className='flex flex-col space-y-1'>
+              <span className='text-lg text-white'>Contact Number</span>
+              <span className='text-zinc-200 underline'>{`(305)-615-5883`}</span>
+            </a>
+            <p className='text-zinc-200'>Availble 7 days a week.</p>
+            <p className='text-zinc-200'>CHAT SUPPORT</p>
+            <p className='text-zinc-200'>Available Monday to Friday</p>
+            <p className='text-zinc-200'>9 am to 5pm IST</p>
+          </div>
+          <div className='flex flex-col'></div>
+          <div className='flex flex-col'></div>
+        </div>
+        {/*  */}
       </div>
     </footer>
   );
--- app/page.js
+++ app/page.js
@@ -526,7 +526,7 @@ export default function Home() {
           </div>
         </div>
       </main>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
