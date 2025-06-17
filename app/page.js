2c475ad4b1ee8c0264442f92efec757226546515
Pankaj Gaikwad <gaikwadpankaj62@gmail.com>
2025-06-17 16:55:55 +0530

    fixes

--- app/page.js
+++ app/page.js
@@ -14,6 +14,7 @@ import {
 } from 'lucide-react';
 import Footer from './components/Footer';
 import Temp from '@/components/Temp';
+import Link from 'next/link';
 
 const poppins = Poppins({
   variable: '--font-geist-sans',
